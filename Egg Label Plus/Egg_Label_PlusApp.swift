91134e9f1d2071eaf7aa352a06deaa988d000132
keshew <artyom.korotckov2017@yandex.ru>
2025-12-09 22:48:33 +0500

    Initial Commit

---
+++ Egg Label Plus/Egg_Label_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_Label_PlusApp.swift
+//  Egg Label Plus
+//
+//  Created by Артём Коротков on 09.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Egg_Label_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Egg Label PlusUITests/Egg_Label_PlusUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Egg_Label_PlusUITestsLaunchTests.swift
+//  Egg Label PlusUITests
+//
+//  Created by Артём Коротков on 09.12.2025.
+//
+
+import XCTest
+
+final class Egg_Label_PlusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
